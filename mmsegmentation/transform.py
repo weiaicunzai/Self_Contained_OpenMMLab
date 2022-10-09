6bfd78852c262b313c37d2ff5ade08be49e6ce48
weiaicunzai <372995411@qq.com>
2022-10-09 23:03:31 +0800

    feat: add RandomRotate

--- mmsegmentation/transform.py
+++ mmsegmentation/transform.py
@@ -640,85 +640,90 @@ class RandomCrop(object):
 
 
 #@PIPELINES.register_module()
-#class RandomRotate(object):
-#    """Rotate the image & seg.
-#    Args:
-#        prob (float): The rotation probability.
-#        degree (float, tuple[float]): Range of degrees to select from. If
-#            degree is a number instead of tuple like (min, max),
-#            the range of degree will be (``-degree``, ``+degree``)
-#        pad_val (float, optional): Padding value of image. Default: 0.
-#        seg_pad_val (float, optional): Padding value of segmentation map.
-#            Default: 255.
-#        center (tuple[float], optional): Center point (w, h) of the rotation in
-#            the source image. If not specified, the center of the image will be
-#            used. Default: None.
-#        auto_bound (bool): Whether to adjust the image size to cover the whole
-#            rotated image. Default: False
-#    """
-#
-#    def __init__(self,
-#                 prob,
-#                 degree,
-#                 pad_val=0,
-#                 seg_pad_val=255,
-#                 center=None,
-#                 auto_bound=False):
-#        self.prob = prob
-#        assert prob >= 0 and prob <= 1
-#        if isinstance(degree, (float, int)):
-#            assert degree > 0, f'degree {degree} should be positive'
-#            self.degree = (-degree, degree)
-#        else:
-#            self.degree = degree
-#        assert len(self.degree) == 2, f'degree {self.degree} should be a ' \
-#                                      f'tuple of (min, max)'
-#        self.pal_val = pad_val
-#        self.seg_pad_val = seg_pad_val
-#        self.center = center
-#        self.auto_bound = auto_bound
-#
-#    def __call__(self, results):
-#        """Call function to rotate image, semantic segmentation maps.
-#        Args:
-#            results (dict): Result dict from loading pipeline.
-#        Returns:
-#            dict: Rotated results.
-#        """
-#
-#        rotate = True if np.random.rand() < self.prob else False
-#        degree = np.random.uniform(min(*self.degree), max(*self.degree))
-#        if rotate:
-#            # rotate image
-#            results['img'] = mmcv.imrotate(
-#                results['img'],
-#                angle=degree,
-#                border_value=self.pal_val,
-#                center=self.center,
-#                auto_bound=self.auto_bound)
-#
-#            # rotate segs
-#            for key in results.get('seg_fields', []):
-#                results[key] = mmcv.imrotate(
-#                    results[key],
-#                    angle=degree,
-#                    border_value=self.seg_pad_val,
-#                    center=self.center,
-#                    auto_bound=self.auto_bound,
-#                    interpolation='nearest')
-#        return results
-#
-#    def __repr__(self):
-#        repr_str = self.__class__.__name__
-#        repr_str += f'(prob={self.prob}, ' \
-#                    f'degree={self.degree}, ' \
-#                    f'pad_val={self.pal_val}, ' \
-#                    f'seg_pad_val={self.seg_pad_val}, ' \
-#                    f'center={self.center}, ' \
-#                    f'auto_bound={self.auto_bound})'
-#        return repr_str
-#
-#
+class RandomRotate(object):
+    """Rotate the image & seg.
+    Args:
+        prob (float): The rotation probability.
+        degree (float, tuple[float]): Range of degrees to select from. If
+            degree is a number instead of tuple like (min, max),
+            the range of degree will be (``-degree``, ``+degree``)
+        pad_val (float, optional): Padding value of image. Default: 0.
+        seg_pad_val (float, optional): Padding value of segmentation map.
+            Default: 255.
+        center (tuple[float], optional): Center point (w, h) of the rotation in
+            the source image. If not specified, the center of the image will be
+            used. Default: None.
+        auto_bound (bool): Whether to adjust the image size to cover the whole
+            rotated image. Default: False
+    """
+
+    def __init__(self,
+                 prob,
+                 degree,
+                 pad_val=0,
+                 seg_pad_val=255,
+                 center=None,
+                 auto_bound=False):
+        self.prob = prob
+        assert prob >= 0 and prob <= 1
+        if isinstance(degree, (float, int)):
+            assert degree > 0, f'degree {degree} should be positive'
+            self.degree = (-degree, degree)
+        else:
+            self.degree = degree
+        assert len(self.degree) == 2, f'degree {self.degree} should be a ' \
+                                      f'tuple of (min, max)'
+        self.pal_val = pad_val
+        self.seg_pad_val = seg_pad_val
+        self.center = center
+        self.auto_bound = auto_bound
+
+    #def __call__(self, results):
+    def __call__(self, img, gt_seg):
+        """Call function to rotate image, semantic segmentation maps.
+        Args:
+            results (dict): Result dict from loading pipeline.
+        Returns:
+            dict: Rotated results.
+        """
+
+        rotate = True if np.random.rand() < self.prob else False
+        degree = np.random.uniform(min(*self.degree), max(*self.degree))
+        if rotate:
+            # rotate image
+            #results['img'] = mmcv.imrotate(
+            img = mmcv.imrotate(
+                #results['img'],
+                img,
+                angle=degree,
+                border_value=self.pal_val,
+                center=self.center,
+                auto_bound=self.auto_bound)
+
+            # rotate segs
+            #for key in results.get('seg_fields', []):
+            gt_seg = mmcv.imrotate(
+                    #results[key],
+                    gt_seg
+                    angle=degree,
+                    border_value=self.seg_pad_val,
+                    center=self.center,
+                    auto_bound=self.auto_bound,
+                    interpolation='nearest')
+        #return results
+        return img, gt_seg
+
+    def __repr__(self):
+        repr_str = self.__class__.__name__
+        repr_str += f'(prob={self.prob}, ' \
+                    f'degree={self.degree}, ' \
+                    f'pad_val={self.pal_val}, ' \
+                    f'seg_pad_val={self.seg_pad_val}, ' \
+                    f'center={self.center}, ' \
+                    f'auto_bound={self.auto_bound})'
+        return repr_str
+
+
 #@PIPELINES.register_module()
 #class RGB2Gray(object):
 #    """Convert RGB image to grayscale image.
