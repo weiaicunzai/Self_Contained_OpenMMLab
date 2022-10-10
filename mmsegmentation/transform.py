6f5c36c728713caebbd6ea319797e68295af20f7
weiaicunzai <372995411@qq.com>
2022-10-10 17:09:50 +0800

    feat: add Compose Class

--- mmsegmentation/transform.py
+++ mmsegmentation/transform.py
@@ -1,5 +1,7 @@
 import copy
 from collections.abc import Sequence
+import collections
+
 
 import mmcv
 import numpy as np
@@ -1497,6 +1499,49 @@ class DefaultFormatBundle(object):
     def __repr__(self):
         return self.__class__.__name__
 
+class Compose(object):
+    """Compose multiple transforms sequentially.
+    Args:
+        transforms (Sequence[dict | callable]): Sequence of transform object or
+            config dict to be composed.
+    """
+
+    def __init__(self, transforms):
+        assert isinstance(transforms, collections.abc.Sequence)
+        self.transforms = []
+        for transform in transforms:
+            # if isinstance(transform, dict):
+                # transform = build_from_cfg(transform, PIPELINES)
+                # self.transforms.append(transform)
+            if callable(transform):
+                self.transforms.append(transform)
+            else:
+                raise TypeError('transform must be callable')
+
+    # def __call__(self, data):
+    def __call__(self, img, gt_seg):
+        """Call function to apply transforms sequentially.
+        Args:
+            data (dict): A result dict contains the data to transform.
+        Returns:
+           dict: Transformed data.
+        """
+
+        for t in self.transforms:
+            # data = t(data)
+            img, gt_seg = t(img, gt_seg)
+            # if data is None:
+                # return None
+        # return data
+        return img, gt_seg
+
+    def __repr__(self):
+        format_string = self.__class__.__name__ + '('
+        for t in self.transforms:
+            format_string += '\n'
+            format_string += f'    {t}'
+        format_string += '\n)'
+        return format_string
 
 
 
