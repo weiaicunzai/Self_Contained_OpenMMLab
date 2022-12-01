24b138fc70143a503e82d5881bc86579c70f110c
weiaicunzai <372995411@qq.com>
2022-12-01 19:50:43 +0800

    feat: add aug_test function

--- segmentation/testaug.py
+++ segmentation/testaug.py
@@ -1,3 +1,4 @@
+
 # Copyright (c) OpenMMLab. All rights reserved.
 import warnings
 import torch
@@ -8,7 +9,6 @@ import torch.nn.functional as F
 
 
 # def slide_inference(img, )
-
 
 def resize(input,
            size=None,
@@ -42,11 +42,14 @@ def slide_inference(img, ori_shape, model, crop_size, stride, num_classes):
     h_stride, w_stride = stride
     #h_crop, w_crop = self.test_cfg.crop_size
     h_crop, w_crop = crop_size
+
+    img = img.unsqueeze(0)
     batch_size, _, h_img, w_img = img.size()
     # out_channels = self.out_channels
     out_channels = num_classes
     h_grids = max(h_img - h_crop + h_stride - 1, 0) // h_stride + 1
     w_grids = max(w_img - w_crop + w_stride - 1, 0) // w_stride + 1
+    # img = img.cuda()
     preds = img.new_zeros((batch_size, out_channels, h_img, w_img))
     count_mat = img.new_zeros((batch_size, 1, h_img, w_img))
     for h_idx in range(h_grids):
@@ -59,7 +62,7 @@ def slide_inference(img, ori_shape, model, crop_size, stride, num_classes):
             x1 = max(x2 - w_crop, 0)
             crop_img = img[:, :, y1:y2, x1:x2]
             # crop_seg_logit = self.encode_decode(crop_img, img_meta)
-            crop_seg_logit = model(crop_img)
+            crop_seg_logit = model(crop_img.cuda()).cpu()
             preds += F.pad(crop_seg_logit,
                            (int(x1), int(preds.shape[3] - x2), int(y1),
                             int(preds.shape[2] - y2)))
@@ -85,6 +88,7 @@ def slide_inference(img, ori_shape, model, crop_size, stride, num_classes):
             # align_corners=align_corners,
             warning=False)
 
+    # preds = preds.squeeze(0)
     return preds
 
 
@@ -116,8 +120,7 @@ def whole_inference(img, ori_shape, model):
         return seg_logit
 
 
-# def inference(self, img, img_meta, rescale, mode):
-def inference(img, ori_shape, flip, flip_direction, mode, model, num_classes, crop_size=None, stride=None):
+def inference(img, ori_shape, flip_direction, mode, model, num_classes, crop_size=None, stride=None):
     """Inference a single image with slide/whole style.
     Args:
         img (Tensor): The input image of shape (N, 3, H, W).
@@ -165,38 +168,31 @@ def inference(img, ori_shape, flip, flip_direction, mode, model, num_classes, cr
 
     # flip
 
-    if flip:
+    # if flip:
         # flip_direction = img_meta[0]['flip_direction']
-        assert flip_direction in ['horizontal', 'vertical']
-        if flip_direction == 'horizontal':
+        # assert flip_direction in ['horizontal', 'vertical']
+    if flip_direction == 'horizontal':
             output = output.flip(dims=(3, ))
-        elif flip_direction == 'vertical':
+        # elif flip_direction == 'vertical':
+    if flip_direction == 'vertical':
             output = output.flip(dims=(2, ))
 
     return output
 
-# def multiscale_test(imgs, ori_shape, mode, crop_size=None, )
-
-
-
-
-
-
-
-def aug_test(imgs, flip_param, ori_shape, model, num_classes, mode, threshold=0.5, crop_size=None, stride=None):
+def aug_test(imgs, flip_direction, ori_shape, model, num_classes, mode, threshold=0.5, crop_size=None, stride=None):
     """aug_test:  test time augmentation with different img_ratios"""
     """aug_data: list of (img, gt_seg) pairs with img_ratios"""
     # print(stride)
 
 
     seg_logit = 0
-    for img, (flip, flip_direction) in zip(imgs, flip_param):
+    for img, flip_direction in zip(imgs, flip_direction):
         # if idx == 0:
             # continue
         # img, gt_seg = data
         cur_seg_logit = inference(
             img=img,
-            flip=flip,
+            # flip=flip,
             flip_direction=flip_direction,
             ori_shape=ori_shape,
             model=model,
@@ -219,124 +215,8 @@ def aug_test(imgs, flip_param, ori_shape, model, num_classes, mode, threshold=0.
 
     seg_pred = seg_pred.cpu().numpy()
 
+    seg_pred = seg_pred.squeeze()
+
     # unravel batch dim
     # seg_pred = list(seg_pred)
     return seg_pred
-
-
-
-
-#class DummyNet(torch.nn.Module):
-#    def __init__(self):
-#        super().__init__()
-#
-#    def forward(self, x):
-#        return x
-#
-#dummy = [
-#            torch.randn(1, 3, 224, 224),
-#            torch.randn(1, 3, 112, 112),
-#            torch.randn(1, 3, 333, 333),
-#            torch.randn(1, 3, 444, 444),
-#            torch.randn(1, 3, 422, 222),
-#         ]
-#
-#flips = [
-#    [True, 'vertical'],
-#    [False, 'horizontal'],
-#    [True, 'vertical'],
-#    [False, 'horizontal'],
-#    [True, 'vertical'],
-#]
-#flip_directions = [
-#
-#]
-# self.dummpy_gt = np.random.rand(333, 333, 3)
-# dummpy_gt = torch.randn(333, 333, 3)
-
-
-# dummy_net = DummyNet()
-
-# aug_test(imgs=dummy, flip_param=flips, ori_shape=[333, 333], model=dummy_net, num_classes=3, mode='slide', crop_size=(100, 100), stride=(50, 50))
-
-
-
-# class Dummy:
-#     def __init__(self):
-#         super().__init__()
-#         self.imgs = 1000
-#         import numpy as np
-
-#         self.dummy = [
-#             np.random.rand(224, 224, 3),
-#             np.random.rand(112, 112, 3),
-#             np.random.rand(333, 333, 3),
-#             np.random.rand(444, 444, 3),
-#             np.random.rand(422, 222, 3),
-#         ]
-
-#         self.dummpy_gt = np.random.rand(333, 333, 3)
-
-
-
-#     def __len__(self):
-#         return self.imgs
-
-
-#     def __getitem__(self, idx):
-
-#         dummpy_gt = self.dummpy_gt
-#         dummpy_gt[0, 0] = idx
-#         return self.dummy, dummpy_gt
-
-
-# from torch.utils.data.dataloader import default_collate
-
-def multiscale_collate(batch):
-
-    res = []
-    for img, gt_seg in batch:
-        res.append([img, gt_seg])
-
-    return res
-
-
-
-# dataset = Dummy()
-
-# dataloader = torch.utils.data.DataLoader(dataset, num_workers=4, batch_size=7, shuffle=False, collate_fn=collate)
-
-# for i in dataloader:
-#     #print(i)
-#     #print(i)
-#     for pair in i:
-#         img, seg = pair
-#         for im in img:
-#             print(im.shape, seg.shape)
-
-#         print()
-#         #print(len(img))
-        #print(seg.shape)
-    #print(i)
-    # print(len(i), type(i))
-    # print(len(i[0]), type(i[0]))
-    # for k in i[0]:
-        # print(k.shape)
-    #print(len(i[1]), type(i[1]))
-    # for img in i[1]:
-        # print(img.shape, img[0, 0])
-
-    # import sys; sys.exit()
-
-
-
-
-# from
-
-#img = torch.randn(333, 333, 3)
-#gt_seg = torch.randn(333, 333, 3)
-
-
-
-
-# def test_aug(img, mode, crop_size, )
